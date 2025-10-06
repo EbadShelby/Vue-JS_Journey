b80f916f0aa8b2da3122de760de53e860e960ec2
EbadShelby <ebadyasser123xd@gmail.com>
2025-10-06 09:08:01 +0800

    change localhost to proxy for deployment

--- job-listing-vue-project/src/components/JobListings.vue
+++ job-listing-vue-project/src/components/JobListings.vue
@@ -16,7 +16,7 @@ const jobs = ref([]);
 
 onMounted(async () => {
   try {
-    const response = await axios.get("http://localhost:5000/jobs");
+    const response = await axios.get("/api/jobs");
     jobs.value = response.data;
   } catch (error) {
     console.error("Error Fetching jobs", error);
